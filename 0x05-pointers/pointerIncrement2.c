cc5224024fd32dc24e8b185174513b5a687abc60
Simon Kachepa <kachepasimon@gmail.com>
2023-07-28 14:30:01 +0000

    Create pointerIncrement2.c

---
+++ 0x05-pointers/pointerIncrement2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description - Program that increment a negative number and change its sign up to 0
+* Return: 0
+*/
+
+int main(void)
+{
+    int arr[] = {10, 11, -1, 56, 67, 5, 4};
+    int *p, *q;
+    p = arr; 
+
+    printf("%d\n", *p);
+    printf("%d %d %d\n", (*p)++, *p++, *++p);
+    
+    return (0);
+}
