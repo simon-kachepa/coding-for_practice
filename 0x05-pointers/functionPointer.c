30a9d4ebe5b6426b7431b8288995ef83f8d3f18a
Simon Kachepa <kachepasimon@gmail.com>
2023-07-28 14:32:32 +0000

    Create functionPointer.c

---
+++ 0x05-pointers/functionPointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int num1, int num2);
+
+int main(void)
+{
+    int result;
+    int (*ptr)(int, int) = sum;
+    result = (*ptr)(30, 20);
+
+    printf("Sum is: %d\n", result);
+    return (0);
+}
+int sum(int num1, int num2)
+{
+    return (num1 + num2);
+}
