709d401601f5fbff7c2847f382f5172846bb8cac
Simon Kachepa <kachepasimon@gmail.com>
2023-08-01 13:58:59 +0000

    Create smallFunction.c

---
+++ 0x03-functions/smallFunction.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void myname(void);
+void reset_to_98(int *n);
+int main(void)
+{
+    int n;
+
+    n = 402;
+    
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    myname();
+    return (0);
+}
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+void myname(void)
+{
+    printf("My name is Simon.\n");
+}
