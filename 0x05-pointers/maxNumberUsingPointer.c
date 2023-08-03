6a1990a8cf84e5be4955ebf199fa7f8cefc83414
Simon Kachepa <kachepasimon@gmail.com>
2023-08-03 14:24:16 +0000

    Create maxNumberUsingPointer.c

---
+++ 0x05-pointers/maxNumberUsingPointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[] = {34, 12, 21, 54, 48, 817};
+
+    int largest = *arr;
+    printf("%lu\n", sizeof(arr) / (sizeof(typeof(arr[0]))));
+
+    for (int index = 0; index < sizeof(arr) / (sizeof(typeof(arr[0]))); index++)
+    {
+        if (*(arr + index) > largest)
+        {
+            largest = *(arr + index);
+        }
+    }
+    printf("Largest: %d\n", largest);
+    return (0);
+}
