b4271d09925115cf365f56b7ca8de83a86ea6481
Simon Kachepa <kachepasimon@gmail.com>
2023-08-07 14:19:10 +0000

    Create  string_intro.c

---
+++ 0x00-basics/string_intro.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Simple intro to strings in C
+ * String - is a sequence of characters.
+ * String is an array of characters
+ * Return: (0)
+*/
+
+int main(void)
+{
+    /**
+     * Declaring string in C
+     * Data_type stringName[size];
+    */
+   char name[100];
+
+   printf("Enter your name: ");
+   scanf("%s", name);
+
+   printf("%.5s\n", name);
+   printf("%10.5s\n", name);
+   puts(name); /*puts automatically adds a new line*/
+    return (0);
+}
