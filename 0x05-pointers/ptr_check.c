649f9398b15a8484e8c839f9df9af5817c0d4799
Simon Kachepa <kachepasimon@gmail.com>
2023-08-11 14:46:45 +0000

    Create ptr_check.c

---
+++ 0x05-pointers/ptr_check.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[5] = {4, 5, 6, 7, 8};
+    int *ptr;
+    ptr = arr;
+
+    printf("%d\n", ptr[4]);
+
+    *ptr = 9;
+    printf("%d\n", *ptr);
+
+    *(ptr+2) = 10;
+    printf("%d\n", *(ptr + 2));
+
+    ptr[4] = 11;
+    printf("%d\n", *(ptr + 4));
+
+
+
+
+    return (0);
+}
