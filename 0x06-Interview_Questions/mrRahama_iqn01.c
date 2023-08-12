7d15f29a6b23917f5c2304407c7d94e08b2c37a1
Simon Kachepa <kachepasimon@gmail.com>
2023-08-12 14:50:04 +0000

    Create mrRahama_iqn01.c

---
+++ 0x06-Interview_Questions/mrRahama_iqn01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Question: What is the output of the following code
+ */
+
+int main(void)
+{
+    int a = 5, b= 3;
+
+    printf("%d\n", ++(a*b+1));
+    return (0);
+}
