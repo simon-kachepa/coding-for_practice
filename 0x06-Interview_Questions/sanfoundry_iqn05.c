215fb2a393312b7d9601457fa3aac9ca3a05feb8
Simon Kachepa <kachepasimon@gmail.com>
2023-08-13 16:43:41 +0000

    Create sanfoundry_iqn05.c

---
+++ 0x06-Interview_Questions/sanfoundry_iqn05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Question: What is the output of the following code
+ */
+
+int main(void)
+{
+	char c1 = 'a', c2 = 'b', c;
+
+	c = c1 + c2;
+	if (c > 'c')
+	{
+		printf("True\n");
+	}
+	else
+	{
+		print("False\n");
+	}
+	return (0);
+}
