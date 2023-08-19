c23e270d1cd734b3a9eaba578c8794da4926bae3
Simon Kachepa <kachepasimon@gmail.com>
2023-08-19 15:04:37 +0000

    Create var_func03.c

---
+++ 0x08-Variadic_functions/var_func03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    print_strings(", ", 2, "Jay", "Hardy");
+    print_strings(", ", 2, "", "Django");
+    return (0);
+}
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    unsigned int index;
+    va_list str;
+    
+    va_start(str, n);
+    for (index = 0; index < n; index++)
+    {
+        printf("%s", s);
+        if ((index != (n - 1)) && (separator != NULL))
+        {
+            printf("%s", separator);
+        }
+    }
+    printf("\n");
+    va_end(str);
+
+}
