fe27d37696bfaad2b488a333dd0da141cb595254
Simon Kachepa <kachepasimon@gmail.com>
2023-08-20 16:08:21 +0000

    Create var_func06.c

---
+++ 0x08-Variadic_functions/var_func06.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_something(int n, ...);
+int main(void)
+{
+    print_something(5, 60, "Hardy", 7.8, 'S', "Capizha");
+    return (0);
+}
+
+void print_something(int n, ...)
+{
+    va_list input;
+    int index;
+
+    va_start(input, n);
+
+    for (index = 0; index < n; index++)
+    {
+        printf("%s", va_arg(input, char *));
+    }
+    printf("\n");
+    va_end(input);
+}
