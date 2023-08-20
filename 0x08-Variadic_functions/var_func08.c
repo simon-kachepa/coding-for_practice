128e81d68344bcd832e11bca75bb8190ef143814
Simon Kachepa <kachepasimon@gmail.com>
2023-08-20 16:10:28 +0000

    Create var_func08.c

---
+++ 0x08-Variadic_functions/var_func08.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_something(char *format, ...);
+int main(void)
+{
+    /* i -> integer*/
+    /* s -> string */
+    /* f -> float */
+    /* c -> character */
+    print_something("ishardyfcs", 60, "Hardy", 7.8, 'S', "Capizha");
+    return (0);
+}
+
+void print_something(char *format, ...)
+{
+    va_list input;
+    int index;
+
+    va_start(input, format);
+
+    for (index = 0; format[index] != '\0'; index++)
+    {
+        if (format[index] == 'i')
+        {
+            printf("%d", va_arg(input, int));
+        }
+        else if (format[index] == 's')
+        {
+            printf("%s", va_arg(input, char *));
+        }
+        else if (format[index] == 'f')
+        {
+            printf("%f", va_arg(input, double));
+        }
+        else if (format[index] == 'c')
+        {
+            printf("%c", va_arg(input, int));
+        }
+        else
+        {
+            printf("%c", format[index]);
+        }
+    }
+    printf("\n");
+    va_end(input);
+}
