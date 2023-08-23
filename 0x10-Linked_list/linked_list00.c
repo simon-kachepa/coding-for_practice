dbce841737307b0202455a521eec92e57679afd9
Simon Kachepa <kachepasimon@gmail.com>
2023-08-23 15:35:25 +0000

    Create linked_list00.c

---
+++ 0x10-Linked_list/linked_list00.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Dscription: Creating a simple node in c
+ * Creating a self referencial structure to represent node elements
+ * Note that a node consist of 2 parts, data part and link part
+*/
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main(void)
+{
+    struct node *head = NULL;
+
+    head = malloc(sizeof(struct node));
+
+    head->data = 90;
+    printf("%d\n", head->data);
+    printf("%p\n", head);
+}
