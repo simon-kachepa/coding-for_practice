cd18384f027d68ca5e91550fa5b7df1e9423a423
Simon Kachepa <kachepasimon@gmail.com>
2023-08-23 15:38:19 +0000

    Create linked_list02.c

---
+++ 0x10-Linked_list/linked_list02.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description: Program that counts the number of nodes of a linked list
+*/
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void node_count(struct node *head);
+
+int main(void)
+{
+    struct node *head = NULL;
+
+    head = malloc(sizeof(struct node));
+    head->data = 10;
+    head->link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 20;
+    current->link = NULL;
+    head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 30;
+    current->link = NULL;
+    head->link->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 40;
+    current->link = NULL;
+    head->link->link->link = current;
+
+    /*Calling function that counts the nodes*/
+    node_count(head);
+    return (0);
+}
+
+void node_count(struct node *head)
+{
+    int count = 0;
+
+    if (head == NULL)
+    {
+        printf("The list is empty\n");
+    }
+    else
+    {
+        struct node *ptr = NULL;
+        ptr = head;
+
+        while(ptr != NULL)
+        {
+            count++;
+            ptr = ptr->link;
+        }
+        printf("The number of nodes is: %d\n", count);
+    }
+}
