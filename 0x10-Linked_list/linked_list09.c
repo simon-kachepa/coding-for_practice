f09a6716fbbb3a0317eee22b040b3ede2bc2b5bf
Simon Kachepa <kachepasimon@gmail.com>
2023-08-26 12:05:19 +0000

    Create linked_list09.c

---
+++ 0x10-Linked_list/linked_list09.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Program that deletes the first node of a linked list
+*/
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void print_data(struct node *head);
+struct node *delete_first_node(struct node *head);
+
+int main(void)
+{
+    struct node *head = NULL;
+
+    head = malloc(sizeof(struct node));
+    head->data = 50;
+    head->link = NULL;
+
+    struct node *current = NULL;
+    current = malloc(sizeof(struct node));
+    current->data = 60;
+    current->link = NULL;
+
+    head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 70;
+    current->link= NULL;
+
+    head->link->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 80;
+    current->link = NULL;
+
+    head->link->link->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 90;
+    current->link = NULL;
+
+    head->link->link->link->link = current;
+
+    /* Calling the print data function to print the data of the existing linked list*/
+    print_data(head);
+
+    /**
+     * Calling the delete head and assign the new head returned to the head
+    */
+   head = delete_first_node(head);
+
+    /**
+     * Printing the new list with the deleted first node 
+    */
+   print_data(head);
+    return (0);
+}
+
+/**
+ * Function that deletes the first node of a linked list
+*/
+struct node *delete_first_node(struct node *head)
+{
+    struct node *ptr;
+    if (head == NULL)
+    {
+        printf("The list is already empty\n");
+    }
+    else
+    {
+        ptr = head;
+        head = head->link;
+        free(ptr);
+        ptr = NULL;
+
+    }
+
+    return (head);
+
+}
+
+/**
+ * Function to print data of the nodes
+*/
+void print_data(struct node *head)
+{
+    struct node *ptr;
+    if (head == NULL)
+    {
+        printf("The linked list is empty");
+    }
+    else
+    {
+        ptr = head;
+        while (ptr != NULL)
+        {
+            printf("%d ", ptr->data);
+            ptr = ptr->link;
+        }
+        printf("\n");
+    }
+}
