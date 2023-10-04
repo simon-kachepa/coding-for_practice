f1ece6e512051e55082990537baecc4baea16adb
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-04 16:28:17 +0200

    Update _struct_intro.c

--- 0x07-structures/_struct_intro.c
+++ 0x07-structures/_struct_intro.c
@@ -19,7 +19,7 @@ int main(void)
     member1.age = 20;
     member1.mark = 78;
 
-    struct Members member2 = {"McDonald Kachepa", 20, 90};
+    struct Members member2 = {"McDonald Kachepa", 20, 80};
 
     printf("%s is %d years old and he got %d\n", member1.name, member1.age, member1.mark);
     printf("%s is %d years old and he got %d\n", member2.name, member2.age, member2.mark);
