98a2d63c5e5c07cf22bb4061b530eebfa1068c39
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-10 23:16:22 +0200

    Update linked_list06.c

--- 0x10-Linked_list/linked_list06.c
+++ 0x10-Linked_list/linked_list06.c
@@ -49,7 +49,7 @@ int main(void)
      */
     
     
-    head = add_node_at_beginning(head, 855);
+    head = add_node_at_beginning(head, 895);
 
     /* Prining the linked list with the newly inserted node*/
     print_data(head);
