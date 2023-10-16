9669f7d21a0eebbccc774100a3dd374fd7895352
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-16 16:11:44 +0200

    Update linked_list07.c

--- 0x10-Linked_list/linked_list07.c
+++ 0x10-Linked_list/linked_list07.c
@@ -49,7 +49,7 @@ int main(void)
      */
     
     
-    add_node_at_beginning(&head, 855);
+    add_node_at_beginning(&head, 825);
 
     /* Prining the linked list with the newly inserted node*/
     print_data(head);
