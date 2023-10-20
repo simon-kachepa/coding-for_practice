3252894912fd2bd464a0942103f600abbd6c1727
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-20 14:41:05 +0200

    Update linked_list08.c

--- 0x10-Linked_list/linked_list08.c
+++ 0x10-Linked_list/linked_list08.c
@@ -31,7 +31,7 @@ int main(void)
     head->link = current;
 
     current = malloc(sizeof(struct node));
-    current->data = 170;
+    current->data = 10;
     current->link= NULL;
 
     head->link->link = current;
