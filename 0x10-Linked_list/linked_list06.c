859134c78eaf6f35a2fb889ba36116c31751d653
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-22 17:00:24 +0200

    Update linked_list06.c

--- 0x10-Linked_list/linked_list06.c
+++ 0x10-Linked_list/linked_list06.c
@@ -19,7 +19,7 @@ int main(void)
     struct node *head = NULL;
 
     head = malloc(sizeof(struct node));
-    head->data = 110;
+    head->data = 11;
     head->link = NULL;
 
     struct node *current = malloc(sizeof(struct node));
