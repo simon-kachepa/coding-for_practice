1d9d556d9d3c9f44487928e24b31b06d60f54b08
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-10-31 15:30:25 +0200

    Update sanfoundry_iqn01.c

--- 0x06-Interview_Questions/sanfoundry_iqn01.c
+++ 0x06-Interview_Questions/sanfoundry_iqn01.c
@@ -2,8 +2,9 @@
 
 /**
 * main - Entry Point
- * Question: What is the output of the following code
- */
+* Question: What is the output of the following code
+* Return: 0 (Success)
+*/
 
 int main(void)
 {
