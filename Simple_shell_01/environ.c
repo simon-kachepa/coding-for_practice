b79eb8783e15ae0245db4b8aa514d7f2762d0e6e
Simon Kachepa <kachepasimon@gmail.com>
2023-12-06 14:04:00 +0000

    Create environ.c

---
+++ Simple_shell_01/environ.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main(void)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	
+	return (0);
+}
