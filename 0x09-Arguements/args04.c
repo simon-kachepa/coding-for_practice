8b9f6a03d0557b5362beabbc011dcb5e6af1464d
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2023-12-11 14:11:38 +0200

    Update args04.c

--- 0x09-Arguements/args04.c
+++ 0x09-Arguements/args04.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[2]);
-    result = num1 * num2;clearerr
+    result = num1 * num2;
 
     printf("%d\n", result);
 }
